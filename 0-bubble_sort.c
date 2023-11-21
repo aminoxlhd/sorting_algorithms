99683a8dedb10c13af145be68d27099ae6776ad5
root <aminoxlhd>
2023-11-21 11:00:49 -0800

    add 0-bubble_sort.c

---
+++ 0-bubble_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - a function that sorts an array of integers in ascending
+* order using the Bubble sort algorithm.
+* @array: array to sort
+* @size: size of the array
+*/
+void bubble_sort(int *array, size_t size)
+{
+	size_t i = 0, j;
+	int temp;
+
+	while (i < size)
+	{
+		for (i = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	i++;
+	}
+}
