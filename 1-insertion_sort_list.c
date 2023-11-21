03437a49c1b69097ab57597aecabc105cdef47d0
root <aminoxlhd>
2023-11-21 15:28:46 -0800

    add 1-insertion_sort_list.c

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,54 @@
+#include "sort.h"
+
+/**
+ * exchange_nodes - Exchange two nodes in a doubly linked list
+ * @head: Pointer to the head of the list
+ * @nodeA: Pointer to the first node
+ * @nodeB: Pointer to the second node
+ */
+void exchange_nodes(listint_t **head, listint_t **nodeA, listint_t *nodeB)
+{
+	listint_t *prevA = (*nodeA)->prev;
+	listint_t *nextB = nodeB->next;
+
+	(*nodeA)->prev = nodeB;
+	nodeB->next = *nodeA;
+	(*nodeA)->next = nextB;
+
+	if (nextB != NULL)
+		nextB->prev = *nodeA;
+
+	if (prevA != NULL)
+		prevA->next = nodeB;
+
+	else
+		*head = nodeB;
+
+	nodeB->prev = prevA;
+	*nodeA = nodeB->prev;
+}
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers in ascending
+ * using the Insertion sort algorithm
+ * @list: Pointer to the head of the list to be sorted
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *prev;
+
+	if (list == NULL || *list == NULL)
+		return;
+
+	current = (*list)->next;
+
+	while (current)
+	{
+	while (prev != NULL && current->n < prev->n)
+	{
+		exchange_nodes(list, &prev, current);
+		print_list((const listint_t *)*list);
+	}
+	current = current->next;
+	}
+}
