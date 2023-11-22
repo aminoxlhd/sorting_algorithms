a7f9da11af2fa33c1e7b7fc158913152a0f50f25
aissa zerrad <jesus.jesus.core@gmail.com>
2023-11-21 18:52:35 -0800

    array

--- print_array.c
+++ print_array.c
@@ -1,13 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
-/** * print_array - Prints an array of integers * * @array: The array to be printed * @size: Number of elements in @array */
+/**
+ * print_array - Prints an array of integers
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ **/
 
 void print_array(const int *array, size_t size)
 {
-	size_t i; i = 0;
+	size_t i;
+
+	i = 0;
+
 	while (array && i < size)
 	{
-		if (i > 0) printf(", ");
+		if (i > 0)
+			printf(", ");
 		printf("%d", array[i]);
 		++i;
 	}
