2e6ec0b93f3a2e32653a60cba0157fed7185c34a
aissa zerrad <jesus.jesus.core@gmail.com>
2023-11-21 18:36:41 -0800

    102-O

--- sort.h
+++ sort.h
@@ -1,4 +1,6 @@
 #include <stddef.h>
+#include <stdlib.h>
+
 /** 
 * struct listint_s - Doubly linked list node 
 * 
@@ -18,3 +20,4 @@ void selection_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
